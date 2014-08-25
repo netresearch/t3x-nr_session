a2ce05062c9d6cd4dc7c06e55804fd8a7d9e193a
Sebastian Mendel <sebastian.mendel@netresearch.de>
2014-08-25 08:03:27 +0200

    Version: 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "nr_session".
  *
- * Auto generated 25-08-2014 07:57
+ * Auto generated 25-08-2014 08:03
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -30,8 +30,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'state' => 'stable',
-	'version' => '2.0.0',
-	'_md5_values_when_last_written' => 'a:7:{s:9:"build.xml";s:4:"3f95";s:9:"ChangeLog";s:4:"0b9d";s:16:"ext_autoload.php";s:4:"8487";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"4f92";s:10:"README.rst";s:4:"b4fb";s:15:"src/Session.php";s:4:"c181";}',
+	'version' => '2.0.1',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"build.xml";s:4:"3f95";s:9:"ChangeLog";s:4:"c96d";s:16:"ext_autoload.php";s:4:"008b";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"cba1";s:10:"README.rst";s:4:"b4fb";s:15:"src/Session.php";s:4:"c181";}',
 );
 
 ?>
